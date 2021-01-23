63585d17bd47fd83a95b9f2c8e06fe86fb7129fe
Skandak99 <69813418+Skandak99@users.noreply.github.com>
2021-01-23 12:06:29 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,51 @@
+// A function is a block of code that performs a specific task
+
+// Math sqaure root, and Math random
+
+// function function_name(){}
+
+// How will we call a function
+// By typing
+// function_name();
+
+// function can be passed using parameters
+
+// function function_name(name){}
+
+// function greet(name){
+    // console.log("hello"+name);
+// }
+
+// greet(hi){}, Hello hi
+
+// let name = prompt("Enter a name")
+
+// function add(a,b)-> a parameter{
+    // console.log (a+b);
+// }
+
+// add(1,1) -> The arguement
+
+// Functions Expresssion
+
+// let x = function(var1, var2, var3){
+    // console.log(var1+var2+var3);
+// }
+// x(1,2,3);
+
+// let x = function(var1, var2, var3){
+    // return var1+var2+var3;
+// }
+// console.log(x(1,2,3))
+
+// if its return, then we use console.log after, if we use console.log at the begining then its cool
+
+// function greet(firstname, secondname){
+//     console.log("Hello"+firstname+secondname);
+//     }
+//     greet(skandakumar, karttikeyan);
+
+function quizpage(){
+    location.href = "quiz.html"; 
+}
+
